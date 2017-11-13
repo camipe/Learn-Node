7bee4651ba2a58d972e6004e202e8a1341d3f067
Micael Persson <micaelpersson@Micaels-MacBook-Air.local>
2017-11-13 20:31:12 +0100

    Finished Module 4 Part 1 and made some linting changes.

--- donuts-shop/.eslintrc.js
+++ donuts-shop/.eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
     "extends": "airbnb-base",
     "plugins": [
         "import"
-    ]
+    ],
+    "rules": {
+        "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+        "no-param-reassign": ["error", {
+            "props": true,
+            "ignorePropertyModificationsFor": ["err"]
+          }]
+      }
 };
--- donuts-shop/handlers/errorHandlers.js
+++ donuts-shop/handlers/errorHandlers.js
@@ -3,19 +3,19 @@
 
   With async/await, you need some way to catch errors
   Instead of using try{} catch(e) {} in each controller, we wrap the function in
-  catchErrors(), catch any errors they throw, and pass it along to our express middleware with next()
+  catchErrors(), catch any errors they throw, and pass it along to our express
+  middleware with next()
 */
 
-exports.catchErrors = (fn) => {
-  return function(req, res, next) {
-    return fn(req, res, next).catch(next);
-  };
+exports.catchErrors = function catchErrors(fn) {
+  return (req, res, next) => fn(req, res, next).catch(next);
 };
 
 /*
   Not Found Error Handler
 
-  If we hit a route that is not found, we mark it as 404 and pass it along to the next error handler to display
+  If we hit a route that is not found, we mark it as 404 and pass it along to the next
+  error handler to display
 */
 exports.notFound = (req, res, next) => {
   const err = new Error('Not Found');
@@ -35,20 +35,22 @@ exports.flashValidationErrors = (err, req, res, next) => {
   const errorKeys = Object.keys(err.errors);
   errorKeys.forEach(key => req.flash('error', err.errors[key].message));
   res.redirect('back');
+  return true;
 };
 
 
 /*
   Development Error Handler
 
-  In development we show good error messages so if we hit a syntax error or any other previously un-handled error, we can show good info on what happened
+  In development we show good error messages so if we hit a syntax error or any other previously
+  un-handled error, we can show good info on what happened
 */
 exports.developmentErrors = (err, req, res, next) => {
   err.stack = err.stack || '';
   const errorDetails = {
     message: err.message,
     status: err.status,
-    stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>')
+    stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>'),
   };
   res.status(err.status || 500);
   res.format({
@@ -56,7 +58,7 @@ exports.developmentErrors = (err, req, res, next) => {
     'text/html': () => {
       res.render('error', errorDetails);
     }, // Form Submit, Reload the page
-    'application/json': () => res.json(errorDetails) // Ajax call, send JSON back
+    'application/json': () => res.json(errorDetails), // Ajax call, send JSON back
   });
 };
 
@@ -70,6 +72,6 @@ exports.productionErrors = (err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
   });
 };
